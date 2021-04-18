723d925de795122bf235b6303a7b507943715ba7
343GuiltySpark-04 <killerdragonxs@gmail.com>
2021-04-17 21:55:13 -0400

    forgot to add the makefile to the last commit
    Signed-off-by: Tristan Adam <killerdragonxs@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PackageArbiter',
+    version='0.1',
+    packages=['database_parser'],
+    url='https://github.com/343GuiltySpark-04/PackageArbiter',
+    license='GPL 2',
+    author='Tristan Adams',
+    author_email='killerdragonxs@gmail.com',
+    description='a package manager for VadamOS'
+)
